abcf6e5664c1b8540cd74104a808e795518beddc
edmundo papi <edmundopierre@yahoo.com>
2020-10-27 00:53:49 +0000

    Automatically backed up by Learn

--- db/migrate/20140212205242_create_rides.rb
+++
@@ -1,9 +0,0 @@
-class CreateRides < ActiveRecord::Migration
-  def change
-    create_table :rides do |t|
-      t.integer :taxi_id
-      t.integer :passenger_id
-      t.timestamps null: false
-    end
-  end
-end
